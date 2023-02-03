a544bc9b2aed149c9180beaca3b9684d4976414d
Execise <vajarinkarapurva>
2023-02-03 17:29:19 +0530

    ok

---
+++ exercise11.py
@@ -0,0 +1,8 @@
+print("How old are You")
+age=input()
+print("how tall are you")
+height=input()
+print("How much do you weight")
+weight=input()
+
+print("so you're %r old, %r tall and %r heavy." %(age,height,weight))
